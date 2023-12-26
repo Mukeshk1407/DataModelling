ee1b36263a08189734884c839d7f09cdca02bf36
MounikaSubash <MounikaSubash@DESKTOP-TQ6D7NE>
2023-12-26 17:50:45 +0530

    Create-Entity-Changes

--- src/app/services/column-input-service.service.ts
+++ src/app/services/column-input-service.service.ts
@@ -7,7 +7,7 @@ import { Observable, catchError, throwError } from 'rxjs';
 })
 
 export class ColumnInputServiceService {
-   private ApiUrlGateWay = 'https://localhost:7093';
+   private ApiUrlGateWay = 'https://localhost:7254';
 
   constructor(private http: HttpClient) { }
   
@@ -16,7 +16,17 @@ export class ColumnInputServiceService {
   }
 
   createTable(request: any): Observable<any> {
-    return this.http.post<any>(`${this.ApiUrlGateWay}/createtable`, request)
+    var databaseDetailsString = localStorage.getItem('databaseDetails');
+    // Parse the JSON string into an object
+    var databaseDetails1 = JSON.parse(databaseDetailsString || '{}');
+
+    // Extract individual values from the object
+    var host = databaseDetails1?.hostname || '';
+    var databaseName = databaseDetails1?.databaseName || '';
+    var username = databaseDetails1?.username || '';
+    var password = databaseDetails1?.password || '';
+    var provider = databaseDetails1?.selectedContent;
+    return this.http.post<any>(`${this.ApiUrlGateWay}/create-table`, request)
       .pipe(
         catchError(error => {
           let errorMessage = 'An error occurred while creating the table.';
